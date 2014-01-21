fb1e9142f24db2e2cbd599095567a32237c40a14
Jenksz <zfiscr@gmail.com>
2014-01-21 11:47:17 -0500

    speakme landing-page

---
+++ form-to-email.php
@@ -0,0 +1,12 @@
+<?php
+  $visitor_email = $_POST['email'];
+?>
+
+<?php
+    $email_from = 'speakme@speakme.com';
+ 
+    $email_subject = "New Form submission!";
+ 
+    $email_body = "You have received a new subscroption from the user $visitor_email.\n".
+                          
+?>
